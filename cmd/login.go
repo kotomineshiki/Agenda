818e78c7a7b863f3c6bcdd6a037779010e527c77
yiqu zhu <kotomineshiki@outlook.com>
2018-10-21 16:22:15 +0800

    login

--- cmd/login.go
+++ cmd/login.go
@@ -20,9 +20,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// registerCmd represents the register command
-var registerCmd = &cobra.Command{
-	Use:   "register",
+// loginCmd represents the login command
+var loginCmd = &cobra.Command{
+	Use:   "login",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,22 +31,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		username,_:=cmd.Flags().GetString("user")
-
-		fmt.Println("register called by"+username)
+		fmt.Println("login called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(registerCmd)
-	registerCmd.Flags().StringP("user","u","Anonymous", "Helpmessagefor UserName")
+	rootCmd.AddCommand(loginCmd)
+
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
