a97ce3a0970860c1a797fb5fc64ec37f174d7627
yiqu zhu <kotomineshiki@outlook.com>
2018-10-25 22:01:52 +0800

    deleteMeeting

--- cmd/createMeeting.go
+++ cmd/createMeeting.go
@@ -1,16 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//输入title participator、starttime、endtime创建一个meeting
 
 package cmd
 
@@ -22,7 +10,7 @@ import (
 
 // createMeetingCmd represents the createMeeting command
 var createMeetingCmd = &cobra.Command{
-	Use:   "createMeeting",
+	Use:   "createMeeting -t [Title] -p [Participator] -s [StartTime] -e [EndTime]",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,7 +19,16 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("createMeeting called")
+
+		title, _ := cmd.Flags().GetString("Title")
+
+		participators, _ := cmd.Flags().GetStringSlice("Participator")
+
+		startTime, _ := cmd.Flags().GetString("StartTime")
+
+		endTime, _ := cmd.Flags().GetString("EndTime")
+		fmt.Println("createMeeting called"+title+participators[0]+startTime+endTime)
+		//判断是否合法
 	},
 }
 
--- cmd/deleteMeeting.go
+++ cmd/deleteMeeting.go
@@ -1,5 +1,4 @@
-//注册一个账户
-
+//输入一个meeting的名字，删除该meeting
 package cmd
 
 import (
@@ -8,9 +7,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// registerCmd represents the register command
-var registerCmd = &cobra.Command{
-	Use:   "register",
+// deleteMeetingCmd represents the deleteMeeting command
+var deleteMeetingCmd = &cobra.Command{
+	Use:   "deleteMeeting",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -19,22 +18,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		username,_:=cmd.Flags().GetString("user")
-	//查看本账户是否已经被注册过
-		fmt.Println("register called by"+username)
+		title, _ := cmd.Flags().GetString("Title")
+		fmt.Println("deleteMeeting called"+title)
+		//查看该Meeting是否存在
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(registerCmd)
-	registerCmd.Flags().StringP("user","u","Anonymous", "Helpmessagefor UserName")
+	rootCmd.AddCommand(deleteMeetingCmd)
+	deleteMeetingCmd.Flags().StringP("Title", "t", "", "meeting title")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// deleteMeetingCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// deleteMeetingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
